413966e539d1e4d31d3b9f079addcd4b1e522af4
khs0806 <boydsa12@naver.com>
2020-04-03 05:14:50 +0900

    home.jsp & HomeContorller 작성, log 설정, css 적용

---
+++ qna/src/test/java/net/slipp/di/HelloWorldMessageProvider.java
@@ -0,0 +1,11 @@
+package net.slipp.di;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Hello World!";
+	}
+
+}
