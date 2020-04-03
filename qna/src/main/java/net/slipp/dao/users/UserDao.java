03f9e438451defcff1390fb6b9aa1eec08ca8d9a
khs0806 <boydsa12@naver.com>
2020-04-03 18:24:57 +0900

    User 컨트롤러, 회원가입 폼 추가

--- qna/src/main/java/net/slipp/dao/users/UserDao.java
+++ qna/src/main/java/net/slipp/dao/users/UserDao.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
@@ -14,11 +16,14 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import net.slipp.domain.users.User;
 
 public class UserDao extends JdbcDaoSupport {
+	private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+	
 	@PostConstruct
 	public void initialize() {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
 		populator.addScript(new ClassPathResource("slipp.sql"));
 		DatabasePopulatorUtils.execute(populator, getDataSource());
+		log.info("database initialized success!");
 	}
 
 	public User findById(String userId) {
