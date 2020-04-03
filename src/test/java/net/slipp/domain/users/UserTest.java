1554aef29298e5794ac75695215f3dd60842d82d
khs0806 <boydsa12@naver.com>
2020-04-04 03:58:15 +0900

    User 데이터에 대한 유효성 검증 코드 추가, 테스트 코드 작성

---
+++ src/test/java/net/slipp/domain/users/UserTest.java
@@ -0,0 +1,40 @@
+package net.slipp.domain.users;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserTest {
+	private static final Logger log = LoggerFactory.getLogger(UserTest.class);
+	
+	private static Validator validator;
+	
+	@BeforeClass
+	public static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	public void userIdWhenIsEmpty() {
+		User user = new User("", "password", "name", "asd@asd.com");
+		Set<ConstraintViolation<User>> constraintViolations = validator.validate(user);
+		assertThat(constraintViolations.size(), is(2));
+		
+		for (ConstraintViolation<User> constraintViolation : constraintViolations) {
+			log.debug("error : {}", constraintViolation.getMessage());
+		}
+	}
+
+}
